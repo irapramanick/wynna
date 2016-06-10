9cfc45348437b10eeea6f224363882948f394b7c
irapramanick <irapramanick@gmail.com>
2016-06-10 15:24:04 -0700

    Add files via upload
    second file

---
+++ ex1.py
@@ -0,0 +1,12 @@
+from graphics import *
+
+win = GraphWin("My Circle", 400, 400)
+c = Circle(Point(200, 200), 100)
+c.draw(win)
+c.setFill('red')
+
+    
+win.setBackground('black')
+                  
+win.getMouse()
+win.close()
